b96d75019ba83b6eb558cdc02510cb5b3e9e61cb
Clara Nahornyj <c.nahornyj@hotmail.fr>
2021-04-22 23:55:41 +0200

    Création fonction renderPhotographers() pour affichage de la liste des photographes

--- main.js
+++ main.js
@@ -1,70 +1,44 @@
 // Ouvrir index.html avec liveServer pour récupération de la data
-let section = document.getElementById("list-photographers");
-
-function getData(){
-  fetch("photographers.json")
-  .then(function(resp) {
-  return resp.json();
-  })
-  .then(function(data) {
-  console.log(data.photographers);
-  let photographers = data.photographers;
-  for(let i = 0; i < photographers.length; i++){
-    console.log(photographers[i].name);
+// Fonction pour récupérer la data dans le fichier json
+async function getPhotographers() {
+  let url = 'photographers.json';
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
   }
-  
-  })
-  .catch(error => {
-    console.log(`${error} - L'objet n'a pas été trouvé.`)
-  })
 }
 
-getData();
+// Fonction pour afficher la liste des photographes
+async function renderPhotographers() {
+  // Récupération de la data
+  let data = await getPhotographers();
+  let list = document.querySelector('#list-photographers');
+  //console.log(data.photographers);
+  let html = '';
+  data.photographers.forEach(photographer => {
+      let htmlSegment = 
+      `
+      <article>
+      <a href="">
+          <img src="assets/Sample Photos/Photographers ID Photos/${photographer.portrait}" class="photographer">
+          <h2>${photographer.name}</h2>
+      </a>
+      <p>${photographer.city}, ${photographer.country}</p>
+      <p class="citation">${photographer.tagline}</p>
+      <p class="priceperday">${photographer.price}€/jour</p>
+      <span>${photographer.tags}</span>
+      </article>`;
 
-/*
-async function allPhotographersList(){
-  const photographers = await getPhotographers();
-  console.log(photographers);
-  let list = document.getElementById("list-photographers");
-  photographers.forEach(photographer => {
-        
-    // Création des balises HTML
-    let item = document.createElement("article");
-    let link = document.createElement("a");
-    let photo = document.createElement("img");
-    let name = document.createElement("h2");
-    let city = document.createElement("p");
-    let country = document.createElement("p");
-    let tagline = document.createElement("p");
-    let price = document.createElement("p");
-    // let tags = document.createElement("?"); 
-
-    // Attribution des classes
-    // Voir pour city + country
-    photo.setAttribute("class","photographer");
-    name.setAttribute("class","name");
-
-    // Hiérarchisation des balises HTML
-    item.appendChild(list);
-    link.appendChild(item);
-    photo.appendChild(link);
-    name.appendChild(link);
-    city.appendChild(item);
-    country.appendChild(item);
-    tagline.appendChild(item);
-    price.appendChild(item);
-    //tags.appendChild(item);
-
-    // Remplissage du contenu texte des balises HTML
-    // photo.src = "assets"+photographers.portrait
-    name.textContent = photographer.photographers.name;
-    
+      html += htmlSegment;
   });
+  list.innerHTML = html;
 }
-*/
+
+renderPhotographers();
 
 let idPhotographer = "";
-
 
 /* récupération du tableau de tags
 let tags = document.querySelectorAll("li");
@@ -77,47 +51,4 @@ for(let i = 0; i < tags.length; i++){
 
 /* au clic sur un tag appliquer un filter avec le textContent de l'élément
 li cliqué
-*/
-
-/*
-async function allPhotographersList(){
-    const photographers = await getPhotographers();
-
-    let list = document.getElementById("list-photographers");
-
-    photographers.forEach(photographer => {
-        
-        // Création des balises HTML
-        let item = document.createElement("article");
-        let link = document.createElement("a");
-        let photo = document.createElement("img");
-        let name = document.createElement("h2");
-        let city = document.createElement("p");
-        let country = document.createElement("p");
-        let tagline = document.createElement("p");
-        let price = document.createElement("p");
-        // let tags = document.createElement("?"); 
-
-        // Attribution des classes
-        // Voir pour city + country
-        photo.setAttribute("class","photographer");
-        name.setAttribute("class","name");
-
-        // Hiérarchisation des balises HTML
-        item.appendChild(liste);
-        link.appendChild(item);
-        photo.appendChild(link);
-        name.appendChild(link);
-        city.appendChild(item);
-        country.appendChild(item);
-        tagline.appendChild(item);
-        price.appendChild(item);
-        //tags.appendChild(item);
-
-        // Remplissage du contenu texte des balises HTML
-        photo.src = "assets"+photographers.portrait
-        
-
-    });
-}
 */
