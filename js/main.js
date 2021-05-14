96831f894b30a4d5bc76669f719b8f7de88f5a7e
Clara Nahornyj <c.nahornyj@hotmail.fr>
2021-05-14 16:07:20 +0200

    Correction du comportement pour tri de la liste des photographes au clic sur li

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ async function renderPhotographers() {
   let list = document.querySelector("#list-photographers");
   //console.log(photographers);
 
-
   // Pour chaque photographe dans le tableau de photographes création des éléments DOM
   photographers.forEach((photographer) => {
     // Création des balises HTML
@@ -61,17 +60,23 @@ async function renderPhotographers() {
   });
 
   function applyFilter(value) {
+    // Récupérer la liste de tous les photographes
     let photographers = data.photographers;
+
     // Modification pour comparaison avec string dans le json
     let category = value.replace("#", "").toLowerCase();
     //console.log(category);
+
     list.innerHTML = "";
+
+    // Parcourir chaque photographe dans le tableau de photographes
     for (let i = 0; i < photographers.length; i++) {
+      // Parcourir chaque tag dans le tableau de tags de chaque photographe
       for (let j = 0; j < photographers[i].tags.length; j++) {
         // Si l'un des tags d'un photographe === category cliqué alors affichage du nom du photographe
         if (photographers[i].tags[j] === category) {
           // Affiche le nombre de photographes qui ont cette catégorie
-          console.log("COUCOU");
+          console.log(photographers[i].tags);
 
           let item = document.createElement("article");
           let link = document.createElement("a");
@@ -89,12 +94,14 @@ async function renderPhotographers() {
           item.appendChild(tagline);
           item.appendChild(price);
 
-          for (let i = 0; i < photographers[i].tags.length; i++) {
+          for(let h = 0; h < photographers[i].tags.length; h++){
+            console.log(photographers[i].tags[h]);
             let tag = document.createElement("span");
-            tag.innerText = `#${photographers[i].tags[j]}`;
+            tag.innerText = `#${photographers[i].tags[h]}`;
             tag.setAttribute("class", "type");
             item.appendChild(tag);
           }
+
 
           photo.src =
             "../assets/Sample Photos/Photographers ID Photos/" +
