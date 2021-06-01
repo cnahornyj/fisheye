84ef9ac93174a001e708c99544f7a3ebe3742379
Clara Nahornyj <c.nahornyj@hotmail.fr>
2021-06-01 23:50:16 +0200

    Suppression des commentaires inutiles

--- js/photographer-page.js
+++ js/photographer-page.js
@@ -392,20 +392,18 @@ async function photographerMedias() {
           document.querySelectorAll('a[href$=".jpg"],a[href$=".mp4"]')
         );
 
-        //console.log(links);
-
         const gallery = links.map((link) => link.getAttribute("href"));
 
-        //console.log(gallery);
         links.forEach((link) =>
           link.addEventListener("click", (e) => {
             e.preventDefault();
-            new Lightbox(e.currentTarget.getAttribute("href"), e.currentTarget, gallery);
+            new Lightbox(
+              e.currentTarget.getAttribute("href"),
+              e.currentTarget,
+              gallery
+            );
           })
         );
-
-        /* TO DO : Gérer le focus sur chaque média et ouvrir la lightbox si touche enter appuyé
-        Problème avec comportement par défaut car c'est la balise href qui est focus ET PAS le média */
       }
 
       /**
@@ -427,7 +425,7 @@ async function photographerMedias() {
        */
       loadImage(url) {
         this.url = url;
-        if (url.endsWith(".mp4")){
+        if (url.endsWith(".mp4")) {
           const video = document.createElement("video");
           const subtitles = document.createElement("track");
           const container = this.element.querySelector(".lightbox__container");
@@ -440,8 +438,10 @@ async function photographerMedias() {
           video.src = url;
           subtitles.setAttribute("kind", "subtitles");
           subtitles.setAttribute("srclang", "fr");
-          subtitles.setAttribute("src", this.reference.children[0].children[0].src);
-          // Intégrer la gestion des eventsKeyUpOnBoard ici ?
+          /*subtitles.setAttribute(
+            "src",
+            this.reference.children[0].children[0].src
+          );*/
         } else {
           const image = new Image();
           const container = this.element.querySelector(".lightbox__container");
@@ -462,6 +462,9 @@ async function photographerMedias() {
           this.prev(e);
         } else if (e.key === "ArrowRight") {
           this.next(e);
+        } else if (e.key === "Tab"){
+          const activeElmt = document.activeElement;
+          console.log(activeElmt);
         }
       }
 
@@ -509,7 +512,7 @@ async function photographerMedias() {
        * @return {HTMLElement}
        */
       buildDOM(url) {
-        const dom = document.createElement("div");
+        const dom = document.createElement("section");
         dom.classList.add("lightbox");
         dom.innerHTML = `<button class="lightbox__close">Fermer</button>
         <button class="lightbox__next">Suivant</button>
@@ -792,131 +795,6 @@ async function photographerMedias() {
 
           // onclick sur l'image ouverture de la light-box
         }
-      }
-    }
-
-    /**
-     * @property {HTMLElement} element
-     * @property {string[]} images Chemins des images de la lightbox
-     * @property {string} url Image actuellement affichée
-     **/
-    class Lightbox {
-      static init() {
-        const links = Array.from(
-          document.querySelectorAll('img[src$=".jpg"],source[src$=".mp4"]')
-        );
-        const gallery = links.map((link) => link.getAttribute("src"));
-        links.forEach((link) =>
-          link.addEventListener("click", (e) => {
-            e.preventDefault();
-            new Lightbox(e.currentTarget.getAttribute("src"), gallery);
-          })
-        );
-      }
-
-      /**
-       * @param {string} url URL de l'image
-       * @param {string[]} images Chemins des images de la lightbox
-       */
-      constructor(url, images) {
-        this.element = this.buildDOM(url);
-        this.images = images;
-        this.loadImage(url);
-        this.onKeyUp = this.onKeyUp.bind(this);
-        document.body.appendChild(this.element);
-        document.addEventListener("keyup", this.onKeyUp);
-      }
-
-      /**
-       * @param {string} url URL de l'image
-       */
-      loadImage(url) {
-        this.url = null;
-        const image = new Image();
-        const container = this.element.querySelector(".lightbox__container");
-        const loader = document.createElement("div");
-        loader.classList.add("lightbox__loader");
-        container.innerHTML = "";
-        container.appendChild(loader);
-        image.onload = () => {
-          container.removeChild(loader);
-          container.appendChild(image);
-          this.url = url;
-        };
-        image.src = url;
-      }
-
-      /**
-       * @param {KeyboardEvent} e
-       */
-      onKeyUp(e) {
-        if (e.key === "Escape") {
-          this.close(e);
-        } else if (e.key === "ArrowLeft") {
-          this.prev(e);
-        } else if (e.key === "ArrowRight") {
-          this.next(e);
-        }
-      }
-
-      /**
-       * Ferme la ligthbox
-       * @param {MouseEvent|KeyboardEvent} e
-       */
-      close(e) {
-        e.preventDefault();
-        this.element.classList.add("fadeOut");
-        window.setTimeout(() => {
-          this.element.parentElement.removeChild(this.element);
-        }, 500);
-        document.removeEventListener("keyup", this.onKeyUp);
-      }
-
-      /**
-       * @param {MouseEvent|KeyboardEvent} e
-       */
-      next(e) {
-        e.preventDefault();
-        let i = this.images.findIndex((image) => image === this.url);
-        if (i === this.images.length - 1) {
-          i = -1;
-        }
-        this.loadImage(this.images[i + 1]);
-      }
-
-      /**
-       * @param {MouseEvent|KeyboardEvent} e
-       */
-      prev(e) {
-        e.preventDefault();
-        let i = this.images.findIndex((image) => image === this.url);
-        if (i === 0) {
-          i = this.images.length;
-        }
-        this.loadImage(this.images[i - 1]);
-      }
-
-      /**
-       * @param {string} url URL de l'image
-       * @return {HTMLElement}
-       */
-      buildDOM(url) {
-        const dom = document.createElement("div");
-        dom.classList.add("lightbox");
-        dom.innerHTML = `<button class="lightbox__close">Fermer</button>
-            <button class="lightbox__next">Suivant</button>
-            <button class="lightbox__prev">Précédent</button>
-            <div class="lightbox__container"></div>`;
-        dom
-          .querySelector(".lightbox__close")
-          .addEventListener("click", this.close.bind(this));
-        dom
-          .querySelector(".lightbox__next")
-          .addEventListener("click", this.next.bind(this));
-        dom
-          .querySelector(".lightbox__prev")
-          .addEventListener("click", this.prev.bind(this));
-        return dom;
       }
     }
   }
