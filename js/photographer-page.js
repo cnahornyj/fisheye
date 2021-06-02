8ba148c8d7f4e52c3378c202311ec0b27327f9f2
Clara Nahornyj <c.nahornyj@hotmail.fr>
2021-06-02 20:22:53 +0200

    A améliorer : accessibilité des médias  lorsque la lightbox est ouverte

--- js/photographer-page.js
+++ js/photographer-page.js
@@ -386,14 +386,14 @@ async function photographerMedias() {
         );
 
         const gallery = links.map((link) => link.getAttribute("href"));
-        const galleryOfReferences = links.map((link) => link.children[0].alt);
+        //const galleryOfReferences = links.map((link) => link.children[0].alt);
 
         links.forEach((link) =>
           link.addEventListener("click", (e) => {
             e.preventDefault();
             new Lightbox(
               e.currentTarget.getAttribute("href"),
-              galleryOfReferences,
+              /*galleryOfReferences,*/
               gallery
             );
           })
@@ -404,11 +404,11 @@ async function photographerMedias() {
        * @param {string} url URL de l'image
        * @param {string[]} images Chemins des images/vidéos de la lightbox
        */
-      constructor(url, reference, images) {
+      constructor(url,/*references,*/images) {
         this.element = this.buildDOM(url);
-        this.reference = reference;
+        //this.references = references;
         this.images = images;
-        this.loadImage(url);
+        this.loadImage(url,/*references*/);
         this.onKeyUp = this.onKeyUp.bind(this);
         document.body.appendChild(this.element);
         document.addEventListener("keyup", this.onKeyUp);
@@ -417,8 +417,9 @@ async function photographerMedias() {
       /**
        * @param {string} url URL de l'image
        */
-      loadImage(url) {
+      loadImage(url/*, reference*/) {
         this.url = url;
+        //this.reference = reference;
         if (url.endsWith(".mp4")) {
           const video = document.createElement("video");
           const subtitles = document.createElement("track");
@@ -457,9 +458,9 @@ async function photographerMedias() {
         let array = url.split("/");
         let realLegend = array[array.length - 1].replaceAll("_", " ");
         if (realLegend.endsWith(".jpg")) {
-          return realLegend.replace(".jpg"," ");
+          return realLegend.replace(".jpg", " ");
         } else {
-          return realLegend.replace(".mp4"," ");
+          return realLegend.replace(".mp4", " ");
         }
       }
 
@@ -498,11 +499,12 @@ async function photographerMedias() {
       next(e) {
         e.preventDefault();
         let i = this.images.findIndex((image) => image === this.url);
-        console.log(i);
+        //let j = this.references.findIndex((reference) => reference === this.reference);
         if (i === this.images.length - 1) {
           i = -1;
         }
-        this.loadImage(this.images[i + 1]);
+        this.loadImage(this.images[i + 1]/*, this.references[j + 1]*/);
+        //console.log(this.reference);
       }
 
       /**
@@ -511,11 +513,12 @@ async function photographerMedias() {
       prev(e) {
         e.preventDefault();
         let i = this.images.findIndex((image) => image === this.url);
-        console.log(i);
+        //let j = this.references.findIndex((reference) => reference === this.reference);
         if (i === 0) {
           i = this.images.length;
         }
-        this.loadImage(this.images[i - 1]);
+        this.loadImage(this.images[i - 1]/*,this.references[j - 1]*/);
+        //console.log(this.reference);
       }
 
       playVideo(e) {
